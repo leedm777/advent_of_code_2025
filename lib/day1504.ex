78f6143f6e5cb9f3879bba90f957573cd5c97396
David M. Lee <leedm777@yahoo.com>
2025-09-01 01:11:52 -0500

    Fixup 2015 day 4 solution

--- lib/day1504.ex
+++ lib/day1504.ex
@@ -1,32 +1,22 @@
 defmodule AoC.Day1504 do
   @behaviour AoC.Solution
-  import AoC.Solution
 
   @impl true
   def solve(:part1, input) do
-    mine_advent_coin(String.trim(input), 1)
+    mine_advent_coin(String.trim(input), 1, 20)
   end
 
   @impl true
   def solve(:part2, input) do
-    mine_advent_coin2(String.trim(input), 1)
+    mine_advent_coin(String.trim(input), 1, 24)
   end
 
-  def mine_advent_coin(secret, val) do
+  def mine_advent_coin(secret, val, num_leading_zero_bits) do
     str = "#{secret}#{val}"
 
     case :crypto.hash(:md5, str) do
-      <<0::20, _::4, _::binary>> -> val
-      _ -> mine_advent_coin(secret, val + 1)
-    end
-  end
-
-  def mine_advent_coin2(secret, val) do
-    str = "#{secret}#{val}"
-
-    case :crypto.hash(:md5, str) do
-      <<0::24, _::binary>> -> val
-      _ -> mine_advent_coin2(secret, val + 1)
+      <<0::size(num_leading_zero_bits), _::bitstring>> -> val
+      _ -> mine_advent_coin(secret, val + 1, num_leading_zero_bits)
     end
   end
 end
