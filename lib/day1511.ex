3640c5f5b56f5b08bc62c24f63a2731f82d5726d
David M. Lee <leedm777@yahoo.com>
2025-09-11 21:59:24 -0500

    2015, day 11, part 1, WIP

---
+++ lib/day1511.ex
@@ -0,0 +1,28 @@
+defmodule AoC.Day1511 do
+  @behaviour AoC.Solution
+  import AoC.Solution
+
+  @impl true
+  def solve(:part1, _input) do
+    "TODO"
+  end
+
+  @impl true
+  def solve(:part2, _input) do
+    "TODO"
+  end
+
+  def has_ascending_letters?(str) do
+    {_, cnt} =
+      Enum.reduce_while(to_charlist(str), {-1, 1}, fn ch, {prior_ch, cnt} ->
+        case {ch - 1, cnt} do
+          {^prior_ch, 2} -> {:halt, {ch, 3}}
+          {^prior_ch, _} -> {:cont, {ch, cnt + 1}}
+          {_, _} -> {:cont, {ch, 1}}
+        end
+      end)
+
+    IO.puts("#{str}, #{cnt}")
+    cnt == 3
+  end
+end
---
+++ test/day1511_test.exs
@@ -0,0 +1,54 @@
+defmodule Day1511Test do
+  use ExUnit.Case, async: true
+
+  doctest AoC.Day1511
+
+  defmodule HasAscendingLettersExamplesTest do
+    use ExUnit.Case,
+      async: true,
+      parameterize:
+        TestHelper.map_example_pairs([
+          {"abc", true},
+          {"abcjieurij", true},
+          {"kdheieejnixyz", true},
+          {"ndhrnekcuensk", false}
+        ])
+
+    test "give the expected output", %{input: input, expected: expected} do
+      actual = AoC.Day1511.has_ascending_letters?(input)
+      assert expected == actual
+    end
+  end
+
+  defmodule PartOneExamplesTest do
+    use ExUnit.Case,
+      async: true,
+      parameterize: TestHelper.map_example_pairs([])
+
+    test "give the expected output", %{input: input, expected: expected} do
+      actual = AoC.Day1511.solve(:part1, input)
+      assert expected == actual
+    end
+  end
+
+  test "part1 final" do
+    actual = AoC.Day1511.solve(:part1, AoC.fetch_input("2015", "11"))
+    assert "TODO" == actual
+  end
+
+  defmodule PartTwoExamplesTest do
+    use ExUnit.Case,
+      async: true,
+      parameterize: TestHelper.map_example_pairs([])
+
+    test "give the expected output", %{input: input, expected: expected} do
+      actual = AoC.Day1511.solve(:part2, input)
+      assert expected == actual
+    end
+  end
+
+  test "part2 final" do
+    actual = AoC.Day1511.solve(:part2, AoC.fetch_input("2015", "11"))
+    assert "TODO" == actual
+  end
+end
