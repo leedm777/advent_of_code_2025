8c304b805fbb454856fdb705b2cb154c13fe82c9
David M. Lee <leedm777@yahoo.com>
2025-12-02 23:54:20 -0600

    2025 day 3, part 1

---
+++ lib/day2503.ex
@@ -0,0 +1,30 @@
+defmodule AoC.Day2503 do
+  @behaviour AoC.Solution
+  import AoC.Solution
+
+  @impl true
+  def solve(:part1, input) do
+    input |> Enum.map(&largest_jolt/1) |> Enum.sum()
+  end
+
+  @impl true
+  def solve(:part2, _input) do
+    "TODO"
+  end
+
+  def largest_jolt(bank) do
+    {a, b} = for <<byte <- bank>>,
+        joltage = byte - ?0,
+        reduce: {0, 0} do
+      acc ->
+        case acc do
+          {first, second} when second > first -> {second, joltage}
+          {first, second} when joltage >= second -> {first, joltage}
+          x -> x
+        end
+    end
+    x = a * 10 + b
+    # IO.puts(:stderr, "#{bank} -> #{x}")
+    x
+  end
+end
---
+++ test/day2503_test.exs
@@ -0,0 +1,49 @@
+defmodule Day2503Test do
+  use ExUnit.Case, async: true
+
+  doctest AoC.Day2503
+
+  defmodule PartOneExamplesTest do
+    use ExUnit.Case,
+      async: true,
+      parameterize:
+        TestHelper.map_example_pairs(
+          [
+            {[
+              "987654321111111",
+              "811111111111119",
+              "234234234234278",
+              "818181911112111",
+            ], 357}
+          ])
+
+    test "give the expected output", %{input: input, expected: expected} do
+      actual = AoC.Day2503.solve(:part1, input)
+      assert expected == actual
+    end
+  end
+
+  test "part1 final" do
+    actual = AoC.Day2503.solve(:part1, AoC.fetch_input("2025", "03"))
+    assert 17408 == actual
+  end
+
+  defmodule PartTwoExamplesTest do
+    use ExUnit.Case,
+      async: true,
+      parameterize:
+        TestHelper.map_example_pairs(
+          [
+          ])
+
+    test "give the expected output", %{input: input, expected: expected} do
+      actual = AoC.Day2503.solve(:part2, input)
+      assert expected == actual
+    end
+  end
+
+  test "part2 final" do
+    actual = AoC.Day2503.solve(:part2, AoC.fetch_input("2025", "03"))
+    assert "TODO" == actual
+  end
+end
