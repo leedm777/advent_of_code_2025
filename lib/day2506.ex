73d4c85d6be129eba101216d8b6d3b2194da186e
David M. Lee <leedm777@yahoo.com>
2025-12-06 10:35:05 -0600

    2025 day 6, part 2

--- lib/day2506.ex
+++ lib/day2506.ex
@@ -19,8 +19,45 @@ defmodule AoC.Day2506 do
   end
 
   @impl true
-  def solve(:part2, _input) do
-    "TODO"
+  def solve(:part2, input) do
+    # things are a bit easier if the ops line is first
+    [ops_line | args_lines] = Enum.reverse(input)
+    better_input = [ops_line | Enum.reverse(args_lines)]
+
+    better_input
+    # add several spaces to we handle the last term, and fix the trimming we do in the original
+    # processing
+    |> Enum.map(fn line -> line <> "    " end)
+    |> Enum.map(&String.graphemes/1)
+    |> Enum.zip_with(&Function.identity/1)
+    |> Enum.reduce({0, 0, " "}, fn [op | val_chars], {grand_total, problem_acc, last_op} ->
+      val_str = val_chars |> Enum.join() |> String.trim()
+
+      cond do
+        # blank columns between problems
+        val_str == "" ->
+          {grand_total + problem_acc, 0, " "}
+
+        # the operator is in the first column of the problem
+        op != " " && last_op == " " && problem_acc == 0 ->
+          val = val_str |> String.to_integer()
+          {grand_total, val, op}
+
+        # accumulate a multiplication answer
+        last_op == "*" ->
+          val = val_str |> String.to_integer()
+          {grand_total, val * problem_acc, last_op}
+
+        # accumulate an addition answer
+        last_op == "+" ->
+          val = val_str |> String.to_integer()
+          {grand_total, val + problem_acc, last_op}
+
+        true ->
+          raise("Should not get here :-/")
+      end
+    end)
+    |> elem(0)
   end
 
   def parse_line(line) do
--- test/day2506_test.exs
+++ test/day2506_test.exs
@@ -9,8 +9,8 @@ defmodule Day2506Test do
       parameterize:
         TestHelper.map_example_pairs([
           {[
-             "123 328  51 64",
-             " 45 64  387 23",
+             "123 328  51 64 ",
+             " 45 64  387 23 ",
              "  6 98  215 314",
              "*   +   *   +  "
            ], 4_277_556}
@@ -24,13 +24,21 @@ defmodule Day2506Test do
 
   test "part1 final" do
     actual = AoC.Day2506.solve(:part1, AoC.fetch_input("2025", "06"))
-    assert 5552221122013 == actual
+    assert 5_552_221_122_013 == actual
   end
 
   defmodule PartTwoExamplesTest do
     use ExUnit.Case,
       async: true,
-      parameterize: TestHelper.map_example_pairs([])
+      parameterize:
+        TestHelper.map_example_pairs([
+          {[
+             "123 328  51 64 ",
+             " 45 64  387 23 ",
+             "  6 98  215 314",
+             "*   +   *   +  "
+           ], 3_263_827}
+        ])
 
     test "give the expected output", %{input: input, expected: expected} do
       actual = AoC.Day2506.solve(:part2, input)
@@ -40,6 +48,7 @@ defmodule Day2506Test do
 
   test "part2 final" do
     actual = AoC.Day2506.solve(:part2, AoC.fetch_input("2025", "06"))
-    assert "TODO" == actual
+    assert 11_371_597_082_440 < actual
+    assert 11_371_597_126_232 == actual
   end
 end
