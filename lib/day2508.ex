c2959b44ede6d677b169ccefebe8810bbefa5c20
David M. Lee <leedm777@yahoo.com>
2025-12-08 15:45:57 -0600

    2025 day 8, part 2

--- lib/day2508.ex
+++ lib/day2508.ex
@@ -36,32 +36,7 @@ defmodule AoC.Day2508 do
         fn {id1, id2, _}, circuits ->
           # IO.puts(:stderr, inspect([id1, id2]))
 
-          Enum.reduce(
-            circuits,
-            {false, nil, []},
-            fn circuit, {found_both, first_circuit, circuits} ->
-              # IO.puts(:stderr, "  " <> inspect([circuit, found_both, first_circuit, circuits]))
-
-              cond do
-                found_both ->
-                  {true, nil, [circuit | circuits]}
-
-                first_circuit != nil &&
-                    (MapSet.member?(circuit, id1) || MapSet.member?(circuit, id2)) ->
-                  {true, nil, [MapSet.union(first_circuit, circuit) | circuits]}
-
-                MapSet.member?(circuit, id1) && MapSet.member?(circuit, id2) ->
-                  {true, nil, [circuit | circuits]}
-
-                MapSet.member?(circuit, id1) || MapSet.member?(circuit, id2) ->
-                  {found_both, circuit, circuits}
-
-                true ->
-                  {found_both, first_circuit, [circuit | circuits]}
-              end
-            end
-          )
-          |> elem(2)
+          attach(circuits, id1, id2)
         end
       )
 
@@ -74,7 +49,68 @@ defmodule AoC.Day2508 do
   end
 
   @impl true
-  def solve(:part2, _input) do
-    "TODO"
+  def solve(:part2, input) do
+    boxes =
+      input
+      |> Enum.map(fn line ->
+        line |> String.split(",") |> Enum.map(&String.to_integer/1)
+      end)
+      |> Enum.with_index()
+
+    distances =
+      for {b1, id1} <- boxes, {b2, id2} <- boxes, id1 < id2 do
+        dist =
+          Enum.zip(b1, b2) |> Enum.map(fn {v1, v2} -> (v1 - v2) * (v1 - v2) end) |> Enum.sum()
+
+        {id1, id2, dist}
+      end
+      |> Enum.sort_by(&elem(&1, 2))
+
+    circuits = boxes |> Enum.map(fn {_, idx} -> MapSet.new([idx]) end)
+
+    {id1, id2} =
+      Enum.reduce_while(distances, circuits, fn {id1, id2, _}, circuits ->
+        new_circuits = attach(circuits, id1, id2)
+
+        if length(new_circuits) == 1 do
+          {:halt, {id1, id2}}
+        else
+          {:cont, new_circuits}
+        end
+      end)
+
+    x1 = Enum.find(boxes, fn {_, idx} -> idx == id1 end) |> elem(0) |> hd()
+    x2 = Enum.find(boxes, fn {_, idx} -> idx == id2 end) |> elem(0) |> hd()
+
+    x1 * x2
+  end
+
+  def attach(circuits, id1, id2) do
+    Enum.reduce(
+      circuits,
+      {false, nil, []},
+      fn circuit, {found_both, first_circuit, circuits} ->
+        # IO.puts(:stderr, "  " <> inspect([circuit, found_both, first_circuit, circuits]))
+
+        cond do
+          found_both ->
+            {true, nil, [circuit | circuits]}
+
+          first_circuit != nil &&
+              (MapSet.member?(circuit, id1) || MapSet.member?(circuit, id2)) ->
+            {true, nil, [MapSet.union(first_circuit, circuit) | circuits]}
+
+          MapSet.member?(circuit, id1) && MapSet.member?(circuit, id2) ->
+            {true, nil, [circuit | circuits]}
+
+          MapSet.member?(circuit, id1) || MapSet.member?(circuit, id2) ->
+            {found_both, circuit, circuits}
+
+          true ->
+            {found_both, first_circuit, [circuit | circuits]}
+        end
+      end
+    )
+    |> elem(2)
   end
 end
--- test/day2508_test.exs
+++ test/day2508_test.exs
@@ -46,7 +46,31 @@ defmodule Day2508Test do
   defmodule PartTwoExamplesTest do
     use ExUnit.Case,
       async: true,
-      parameterize: TestHelper.map_example_pairs([])
+      parameterize:
+        TestHelper.map_example_pairs([
+          {[
+             "162,817,812",
+             "57,618,57",
+             "906,360,560",
+             "592,479,940",
+             "352,342,300",
+             "466,668,158",
+             "542,29,236",
+             "431,825,988",
+             "739,650,466",
+             "52,470,668",
+             "216,146,977",
+             "819,987,18",
+             "117,168,530",
+             "805,96,715",
+             "346,949,466",
+             "970,615,88",
+             "941,993,340",
+             "862,61,35",
+             "984,92,344",
+             "425,690,689"
+           ], 25272}
+        ])
 
     test "give the expected output", %{input: input, expected: expected} do
       actual = AoC.Day2508.solve(:part2, input)
@@ -56,6 +80,6 @@ defmodule Day2508Test do
 
   test "part2 final" do
     actual = AoC.Day2508.solve(:part2, AoC.fetch_input("2025", "08"))
-    assert "TODO" == actual
+    assert 25325968 == actual
   end
 end
