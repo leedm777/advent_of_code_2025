3540fd5a2d484fe68ee23d83cf9e544fe3085e05
David M. Lee <leedm777@yahoo.com>
2025-12-13 16:34:06 -0600

    2025 day 10, part 2
    Hacky recursive solution that probably works, but is gonna take hours.

--- lib/day2510.ex
+++ lib/day2510.ex
@@ -11,8 +11,12 @@ defmodule AoC.Day2510 do
   end
 
   @impl true
-  def solve(:part2, _input) do
-    "TODO"
+  def solve(:part2, input) do
+    input
+    |> Enum.map(&parse_line_joltage/1)
+    |> Enum.map(&solve_joltage/1)
+    |> Enum.map(&length/1)
+    |> Enum.sum()
   end
 
   def parse_line(line) do
@@ -65,5 +69,73 @@ defmodule AoC.Day2510 do
       {toggles, Bitwise.bxor(current_indicators, next_toggle), [next_toggle | toggled]}
       | flip_one_of_each(toggles, current_indicators, toggled)
     ]
+  end
+
+  def parse_line_joltage(line) do
+    [_ | rest] = String.split(line)
+    {joltage, buttons} = List.pop_at(rest, -1)
+    {parse_joltage(joltage), Enum.map(buttons, &parse_joltage/1)}
+  end
+
+  def parse_joltage(str) do
+    toggles_str = String.slice(str, 1, String.length(str) - 2)
+
+    toggles_str
+    |> String.split(",")
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  def solve_joltage({wanted_joltage, buttons}) do
+    init_joltage = Enum.map(wanted_joltage, fn _ -> 0 end)
+    solve_joltage(wanted_joltage, buttons, [{init_joltage, []}], MapSet.new())
+  end
+
+  def solve_joltage(wanted_joltage, _buttons, [{joltage, keypresses} | _queue], _visited)
+      when wanted_joltage == joltage do
+    keypresses
+  end
+
+  def solve_joltage(_wanted_joltage, _buttons, [], _visited) do
+    nil
+  end
+
+  def solve_joltage(wanted_joltage, buttons, [{joltage, keypresses} | queue], visited) do
+    # IO.puts(:stderr, inspect(%{q_length: length(queue), v_length: MapSet.size(visited)}))
+
+    cond do
+      # we've been here before; skip
+      MapSet.member?(visited, joltage) ->
+        # IO.puts(:stderr, "  skip revisit")
+        solve_joltage(wanted_joltage, buttons, queue, visited)
+
+      # keep searching
+      true ->
+        next_visited = MapSet.put(visited, joltage)
+
+        q2 =
+          Enum.map(buttons, &press_button(&1, joltage, keypresses))
+          |> Enum.filter(fn {next_joltage, _} ->
+            !MapSet.member?(visited, next_joltage) &&
+              Enum.zip(wanted_joltage, next_joltage) |> Enum.all?(fn {w, j} -> j <= w end)
+          end)
+
+        case Enum.find(q2, fn {next_joltage, _} -> next_joltage == wanted_joltage end) do
+          nil ->
+            solve_joltage(wanted_joltage, buttons, queue ++ q2, next_visited)
+
+          soln ->
+            IO.puts("Skipping #{length(queue)} queue items")
+            solve_joltage(wanted_joltage, buttons, [soln], next_visited)
+        end
+    end
+  end
+
+  def press_button(button, joltage, keypresses) do
+    j =
+      Enum.reduce(button, joltage, fn idx, j ->
+        List.update_at(j, idx, &(&1 + 1))
+      end)
+
+    {j, [button | keypresses]}
   end
 end
--- test/day2510_test.exs
+++ test/day2510_test.exs
@@ -30,7 +30,14 @@ defmodule Day2510Test do
   defmodule PartTwoExamplesTest do
     use ExUnit.Case,
       async: true,
-      parameterize: TestHelper.map_example_pairs([])
+      parameterize:
+        TestHelper.map_example_pairs([
+          {[
+             "[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}",
+             "[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}",
+             "[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}"
+           ], 33}
+        ])
 
     test "give the expected output", %{input: input, expected: expected} do
       actual = AoC.Day2510.solve(:part2, input)
@@ -38,6 +45,7 @@ defmodule Day2510Test do
     end
   end
 
+  @tag timeout: :infinity
   test "part2 final" do
     actual = AoC.Day2510.solve(:part2, AoC.fetch_input("2025", "10"))
     assert "TODO" == actual
